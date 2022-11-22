7408d4317a421b9e2e0a672076cb046a7aa131da
Gerardo Vazquez <gvzq@hotmail.com>
2022-11-21 19:59:16 -0600

    feat: tech stack endpoint
    Analyze endpoint working for valid http or https urls.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('./src/middleware/logger').middleware;
 const log = require('./src/middleware/logger').default;
+const apiRouter = require('./src/routes/api');
 
 const port = process.env.PORT;
 const app = express();
@@ -9,8 +10,10 @@ const app = express();
 app.use(logger);
 app.use(express.json());
 
+app.use('/api', apiRouter);
+
 app.get('/*', (req, res) => {
-  res.sendStatus(200);
+  res.sendStatus(404);
 });
 
 app.listen(port, (err) => {
@@ -18,5 +21,5 @@ app.listen(port, (err) => {
     log.error('Failure to launch server');
     return;
   }
-  log.info(`Listening on port ${port}`);
+  log.info('App started');
 });
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,49 @@
+require('dotenv').config();
 const express = require('express');
+const Wappalyzer = require('wappalyzer');
+const log = require('../middleware/logger').default;
 
 const router = express.Router();
+
+// TODO: create a static user, let others register in memory.
+// const users = [{
+//   _id: 1,
+//   api_key: process.env.API_KEY,
+//   username: 'test',
+//   usage: [{ date: '2022-10-10', count: 0 }],
+// }];
+
+router.post('/register', (req, res) => {
+  const { username } = req.body;
+  //  TODO: const user = createUser(username);
+  res.status(201).send({ data: username });
+});
+
+// get list of tech used by website
+router.get('/analyze/:website', async (req, res) => {
+  const { website } = req.params;
+  const isValidWebsite = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_+.~#?&//=]*)/gm;
+  let tech = [];
+
+  if (!website.match(isValidWebsite)) {
+    res.sendStatus(206);
+  }
+
+  const wappalyzer = new Wappalyzer();
+  try {
+    await wappalyzer.init();
+    const site = await wappalyzer.open(website);
+    site.on('error', (e) => {
+      log.error(`wappalyzer error: ${e}`);
+    });
+    tech = await site.analyze();
+    await wappalyzer.destroy();
+  } catch (error) {
+    log.error(error);
+  }
+
+  tech = tech?.technologies.filter((elem) => elem.confidence === 100);
+  res.status(200).send(tech);
+});
 
 module.exports = router;
